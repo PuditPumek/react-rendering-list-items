76fc3e5c6b80ae6ddb83042a870f3f23a194393e
Pumek <mekawinlnwza@gmail.com>
2026-04-15 16:58:56 +0700

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import MovieList from "./components/MovieList";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        {MovieList.map((movie, index) => (
+          <div key={index} className="movie-card">
+            <h2 className="Title: ">{movie.title}</h2>
+            <p className="Year: ">{movie.year}</p>
+            <p className="Runtime: ">{movie.runtime}</p>
+            <p className="Director: ">{movie.director}</p>
+            <img src={movie.image} alt={movie.title} className="movie-poster" />
+            <p className="Genres: ">{movie.genres.join(", ")}</p>
+            <p className="IMDb Rating: ">{movie.imdbRating}</p>
+            <p className="IMDb Votes: ">{movie.imdbVotes}</p>            
+          </div>
+        ))}
       </section>
     </div>
   );
